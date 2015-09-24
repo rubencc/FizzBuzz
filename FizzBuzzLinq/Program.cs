bdaef1860d6ed31c1f1a8bafb615f44c0e0b6441
rubencc <rubencc@gmail.com>
2015-09-24 20:19:21 +0200

    Añadid0 proyecto para linq

---
+++ FizzBuzzLinq/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = new List<int>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                integers.Add(i);
+            }
+
+            integers.ForEach(item => Console.Write(string.Format(" {0}", item)));
+            Console.ReadKey();
+
+            Console.WriteLine(String.Empty);
+
+            //Funcion que use Linq
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
