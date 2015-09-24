79e1d9b76ead04a9d5e4cc4103f26cd1ea84dac1
rubencc <rubencc@gmail.com>
2015-09-24 19:54:13 +0200

    Kata Fizz Buzz

---
+++ FizzBuzzConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzConsole
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            string numbers = BuildNumbers();
+
+            Console.WriteLine(numbers);
+            Console.ReadKey();
+
+        }
+
+        private static string BuildNumbers()
+        {
+            StringBuilder sb = new StringBuilder(); 
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sb.Append(i.ToString());
+                if(i != 100)
+                    sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
